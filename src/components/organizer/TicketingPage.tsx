be1e67aa3219228ac007a0fd5d036cb40e76fef6
Tanmay-ops4210 <berosgang@gmail.com>
2025-08-15 13:02:35 +0530

    Fix JSX structure in TicketingPage component

--- src/components/organizer/TicketingPage.tsx
+++ src/components/organizer/TicketingPage.tsx
@@ -184,6 +184,7 @@ const TicketingPage: React.FC = () => {
     );
   }
   return (
+    <>
     <div className="min-h-screen bg-gray-50 pt-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {/* Header */}
@@ -551,9 +552,9 @@ const TicketingPage: React.FC = () => {
             </div>
           </div>
         )}
-        </div>
-      </div>
-    </div>
+       </div>
+     </div>
+    </>
   );
 };
 
