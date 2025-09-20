b3a4b6497199c4f7a89fbebb3507f00caa450a0f
Tanmay-ops4210 <berosgang@gmail.com>
2025-09-20 20:46:18 +0530

    Create database.ts

---
+++ src/types/database.ts
@@ -0,0 +1,20 @@
+export interface AppUser {
+  id: string;
+  username: string;
+  email: string;
+  created_at: string;
+  updated_at?: string;
+  role?: string;
+  status?: string;
+}
+
+export interface Event {
+  id: string;
+  event_name: string;
+  event_type: string;
+  event_date?: string;
+  user_id: string;
+  expected_attendees: number;
+  current_attendees?: number;
+  app_users?: AppUser;
+}
